ab0aa191aefa7836ba3583236bd14e64ab1cbe79
J. van Harn <jeffrey.wizkid@gmail.com>
2015-07-25 11:33:29 +0200

    Converting the event class file names to lowercase. Step 1 of 2.

--- system/framework/event/observerlc.php
+++ system/framework/event/observerlc.php
@@ -3,23 +3,10 @@
  * Observer Pattern - Observer Event Interface
  * 
  * @package		Quark-Framework
- * @version		$Id: Observer.php 69 2013-01-24 15:14:45Z Jeffrey $
- * @author		Jeffrey van Harn <support@pagetreecms.org>
+ * @author		Jeffrey van Harn <jeffrey at lessthanthree.nl>
  * @since		Oktober 23, 2011
- * @copyright	Copyright (C) 2011 Jeffrey van Harn. All rights reserved.
+ * @copyright	Copyright (C) 2011-2015 Jeffrey van Harn. All rights reserved.
  * @license		http://opensource.org/licenses/gpl-3.0.html GNU Public License Version 3
- * 
- * Copyright (C) 2011 Jeffrey van Harn
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License (License.txt) for more details.
  */
 
 // Define Namespace
